347793bf5fa5308bea7279ce64f976c81a08028f
Marcelo Augusto <margustoadm@hotmail.com>
2026-01-28 15:46:11 -0300

    Criando uma Calculadora

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HelloWorldComponent } from './helloworld/helloworld.component'
+import { Calculadora } from './calculadora/calculadora'
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloWorldComponent],
+  imports: [Calculadora],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
