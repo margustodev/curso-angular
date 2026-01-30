803c7d262c57e4f07e23d9112bca66b844aae55e
Marcelo Augusto <margustoadm@hotmail.com>
2026-01-30 14:10:39 -0300

    Formulario de adição de itens

--- src/app/lista-compras/lista-compras.ts
+++ src/app/lista-compras/lista-compras.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './lista-compras.html',
   styleUrl: './lista-compras.scss',
 })
 export class ListaCompras {
+  item: string = '';
 
+  adicionarItem() {
+    console.log("Item recebido: " + this.item);
+    this.item = '';
+  }
 }
